ddfd5be3b28e1ae55f35d92bf2403dd5e225636a
nedimbatilovic <nedimco2006@gmail.com>
2021-12-21 20:31:16 +0100

    update

---
+++ vezba 27/app.js
@@ -0,0 +1,26 @@
+// const unos = document.querySelector("#unos");
+// const btn = document.querySelector(".aha");
+// const div = document.querySelector(".div1");
+
+// btn.addEventListener("click", () => {
+//     ime = unos.value;
+//     h1 = document.createElement("h1");
+//     h1.innerText = ime;
+//     div.appendChild(h1);
+// });
+
+// const container = document.querySelector(".container");
+// const num = document.querySelector(".number");
+// const btn = document.querySelector(".button1");
+
+// btn.addEventListener("click", () => {
+//     broj = num.value;
+//     if (parseInt(broj) > 1 && parseInt(broj) < 10) {
+//         h4 = document.createElement("h4");
+//         container.appendChild(h4);
+//         h4.innerText = "valid";
+//     } else {
+//         h4.innerText = "invalid";
+//         container.appendChild(h4);
+//     }
+// });
